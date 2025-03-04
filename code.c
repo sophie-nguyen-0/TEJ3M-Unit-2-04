00e03652290555ea73276486460cab9f0eff90ed
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2025-03-04 09:37:47 -0500

    rfg

--- code.c
+++ code.c
@@ -1,54 +1,57 @@
 /*
  * Created by Sophie N
- * Created on March 4, 2025
- * 
+ * Created on March 4, 202PIN_5
+ *
  * Turns an RGB LED on, showing the different possible colors. 
  */
 
+ const int PIN_5 = 5;
+ const int PIN_6 = 6;
+ const int PIN_7 = 7;
  int blink_time = 1000;
   
  void setup() 
 {
-   pinMode(5, OUTPUT); // GREEN
-   pinMode(6, OUTPUT); // BLUE
-   pinMode(7, OUTPUT); // RED
+   pinMode(PIN_5, OUTPUT); // GREEN
+   pinMode(PIN_6, OUTPUT); // BLUE
+   pinMode(PIN_7, OUTPUT); // RED
  }
  
  void loop() 
 {
-   digitalWrite(5, HIGH);  // GREEN
-   digitalWrite(6, LOW); 
-   digitalWrite(7, LOW);  
+   digitalWrite(PIN_5, HIGH);  // GREEN
+   digitalWrite(PIN_6, LOW); 
+   digitalWrite(PIN_7, LOW);  
    delay(blink_time);  
 
-   digitalWrite(5, LOW); // BLUE
-   digitalWrite(6, HIGH);
-   digitalWrite(7, LOW);
+   digitalWrite(PIN_5, LOW); // BLUE
+   digitalWrite(PIN_6, HIGH);
+   digitalWrite(PIN_7, LOW);
    delay(blink_time);
 
-   digitalWrite(5, LOW); // RED
-   digitalWrite(6, LOW);
-   digitalWrite(7, HIGH);
+   digitalWrite(PIN_5, LOW); // RED
+   digitalWrite(PIN_6, LOW);
+   digitalWrite(PIN_7, HIGH);
    delay(blink_time);
 
-   digitalWrite(5, HIGH); // YELLOW
-   digitalWrite(6, LOW);
-   digitalWrite(7, HIGH);
+   digitalWrite(PIN_5, HIGH); // YELLOW
+   digitalWrite(PIN_6, LOW);
+   digitalWrite(PIN_7, HIGH);
    delay(blink_time);
 
-   digitalWrite(5, HIGH);  // CYAN
-   digitalWrite(6, HIGH);
-   digitalWrite(7, LOW);
+   digitalWrite(PIN_5, HIGH);  // CYAN
+   digitalWrite(PIN_6, HIGH);
+   digitalWrite(PIN_7, LOW);
    delay(blink_time);
 
-   digitalWrite(5, LOW);  // MAGENTA
-   digitalWrite(6, HIGH);
-   digitalWrite(7, HIGH);
+   digitalWrite(PIN_5, LOW);  // MAGENTA
+   digitalWrite(PIN_6, HIGH);
+   digitalWrite(PIN_7, HIGH);
    delay(blink_time);
    
-   digitalWrite(5, HIGH);  // WHITE
-   digitalWrite(6, HIGH);
-   digitalWrite(7, HIGH);
+   digitalWrite(PIN_5, HIGH);  // WHITE
+   digitalWrite(PIN_6, HIGH);
+   digitalWrite(PIN_7, HIGH);
    delay(blink_time);
 
  }
